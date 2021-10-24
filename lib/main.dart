898650f569e395425d339c9cf7c5d69a577cda8d
daisuke <grass536@gmail.com>
2021-10-24 14:02:19 +0900

    add Licence

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_test_application/service/initialize.dart';
 import 'package:riverpod_test_application/view/home/home_screen.dart';
-import 'package:riverpod_test_application/view/list/notification_add_screen.dart';
+import 'package:riverpod_test_application/view/notification_add/notification_add_screen.dart';
 import 'package:riverpod_test_application/view/setting/setting_screen.dart';
 import 'package:riverpod_test_application/view/subscribe/subscribe_sceen.dart';
 
@@ -13,12 +14,16 @@ Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
-
+  LicenseRegistry.addLicense(() async* {
+    final license =
+        await rootBundle.loadString('assets/licence/google_font/poppins.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   await Future.wait([
     // initialize.firebaseEmulator(isEmulator: isEmulator),
     // initialize.firebaseLoginCheck(),
     initialize.configureLocalTimeZone(),
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
   ]);
 
   runApp(
