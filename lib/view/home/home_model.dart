7a1b67abacd1c86ecc8f09f0f6d2366e6885c82d
daisuke <grass536@gmail.com>
2021-10-24 14:04:29 +0900

    update pathの変更

--- lib/view/home/home_model.dart
+++ lib/view/home/home_model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod_test_application/view/list/notification_add_screen.dart';
+import 'package:riverpod_test_application/view/notification_add/notification_add_screen.dart';
 import 'package:riverpod_test_application/view/subscribe/subscribe_sceen.dart';
 
 final bottomIndex = StateProvider<int>((ref) => 0);
